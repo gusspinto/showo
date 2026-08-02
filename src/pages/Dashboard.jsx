667cf4b247730feac645be7dc9ea45bc0bcd404d
Gustavo Pinto <120883172+gusspinto@users.noreply.github.com>
2026-08-02 15:20:34 +0100

    fix: remove dead ScoreHero import that broke Vercel build

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,11 +20,9 @@ import {
   EmptyState, PageLayout, PageHeader, ProgressBar, ProgressRing,
 } from '../components/ui'
 import MonthCalendar from '../components/dashboard/MonthCalendar'
-import ScoreHero from '../components/dashboard/ScoreHero'
 import AddReminderModal from '../components/dashboard/AddReminderModal'
 import CalendarSyncModal from '../components/dashboard/CalendarSyncModal'
 import '../components/dashboard/MonthCalendar.css'
-import '../components/dashboard/ScoreHero.css'
 import './Dashboard.css'
 import StudentDashboard from './StudentDashboard'
 
